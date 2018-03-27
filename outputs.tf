84619aaabb3ee107917272354da76c1f618989c8
limed <limed@mozilla.com>
2018-03-27 10:07:53 -0700

    Add some additional outputs

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,27 @@ output "vpn_gateway_id" {
 output "vpn_customer_gateaway_id" {
   value = "${element(concat(aws_customer_gateway.customer_gateway.*.id, list("")), 0)}"
 }
+
+output "vpn_tunnel1_address" {
+  value = "${element(concat(aws_vpn_connection.main.*.tunnel1_address, list("")), 0)}"
+}
+
+output "vpn_tunnel1_preshared_key" {
+  value = "${element(concat(aws_vpn_connection.main.*.tunnel1_preshared_key, list("")), 0)}"
+}
+
+output "vpn_tunnel1_bgp_asn" {
+  value = "${element(concat(aws_vpn_connection.main.*.tunnel1_bgp_asn, list("")), 0)}"
+}
+
+output "vpn_tunnel2_address" {
+  value = "${element(concat(aws_vpn_connection.main.*.tunnel2_address, list("")), 0)}"
+}
+
+output "vpn_tunnel2_preshared_key" {
+  value = "${element(concat(aws_vpn_connection.main.*.tunnel2_preshared_key, list("")), 0)}"
+}
+
+output "vpn_tunnel2_bgp_asn" {
+  value = "${element(concat(aws_vpn_connection.main.*.tunnel2_bgp_asn, list("")), 0)}"
+}
